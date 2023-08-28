c6f9699969bd59ad9af895f32df84a1e6e1b724a
Leonardo <leonardosh1327@gmail.com>
2023-08-28 18:30:23 -0300

    #Projeto criado localmente e enviado ao github #Adicionado o modelo MVC #Classes: Person, Controller e PersonView criadas e implementadas

---
+++ maxSystem-SA5/src/main/java/org/example/controller/Controller.java
@@ -0,0 +1,55 @@
+package com.example.appwebsenai.controller;
+
+import com.example.appwebsenai.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Controller {
+
+    @Autowired
+    private PersonRep personRep;
+    private List<Person> persons = new ArrayList<>();
+    private int id = 0;
+
+    public Person findClient(String name){
+        List<Person> persons = (List<Person>) personRep.findAll();
+        for(Person p : persons){
+            if(persons.getName().equals(name)){
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    public Person addClient(String name, String sexo){
+        Person p = new Person();
+        p.setName(name);
+        p.setSexo(sexo);
+        id++;
+        p.setId(id);
+        personRep.save(p);
+        return p;
+    }
+
+    public void removeClient(String name){
+        Person p = findPerson(name);
+        personRep.delete(p);
+    }
+
+    public Person editClient(String name, String sexo){
+        Person p = findPerson(name);
+        p.setSexo(sexo);
+        personRep.save(p);
+        return p;
+    }
+
+    public List<Person> listAll(){
+        return (List<Person>)personRep.findAll();
+    }
+
+}
